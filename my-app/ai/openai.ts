4822a55cc755ae982fdedef492b2444012419a8b
VitorLostadaC <vitorlostada@hotmail.com>
2025-05-20 07:22:27 -0300

    feat: create a new project

--- my-app/ai/openai.ts
+++ my-app/ai/openai.ts
@@ -1,5 +1,5 @@
 import { createOpenAI } from '@ai-sdk/openai'
-import { env } from '../env'
+import { env } from '../../stript-duck-test/src/renderer/src/env'
 
 export const openai = createOpenAI({
   apiKey: env.VITE_OPENAI_API_KEY
