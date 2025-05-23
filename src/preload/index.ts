cf531e01e9355ed16385bebceacf1e6708c89d3f
VitorLostadaC <vitorlostada@hotmail.com>
2025-05-23 15:56:39 -0300

    feat: add screenshot funciton

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,8 +1,11 @@
-import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge, ipcRenderer } from 'electron'
 
 // Custom APIs for renderer
-const api = {}
+
+const api = {
+  takeScreenshot: () => ipcRenderer.invoke('take-screenshot')
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
