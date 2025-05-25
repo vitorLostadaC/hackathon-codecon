4fad35a5d0f91c8157d279306bb35c73d9532113
VitorLostadaC <vitorlostada@hotmail.com>
2025-05-25 13:22:01 -0300

    refactor: adjust the prompt engineering

--- src/renderer/src/ai/main.ts
+++ src/renderer/src/ai/main.ts
@@ -3,6 +3,8 @@
 import { generateText } from 'ai'
 import { openai } from '../services/openai'
 
+const safeMode = false
+
 const memories: {
   role: 'user' | 'assistant'
   content: string
@@ -46,7 +48,7 @@ const readImage = async (base64Image: string) => {
 
 export const generateMemory = async (message: string) => {
   const { text } = await generateText({
-    model: openai('gpt-4.1'),
+    model: openai('gpt-4.1-nano'),
     maxTokens: 200,
     messages: [
       {
@@ -65,34 +67,31 @@ export const generateMemory = async (message: string) => {
 }
 
 export const getTemporaryMessage = async () => {
-  console.log('\x1b[36m Starting...')
-
-  console.log('\x1b[36m runing intercal')
-
   console.log('\x1b[36m taking screenshot')
+
   const base64Image = await window.api.takeScreenshot()
-  console.log('\x1b[36m gotten screenshot')
 
   console.log('\x1b[36m reading image')
   const imageTranscription = await readImage(base64Image)
-  console.log('\x1b[36m gotten image transcription')
-
-  console.log('\x1b[36m image transcription: ', imageTranscription)
+  console.log('\x1b[33m image transcription: ', imageTranscription)
 
   console.log('\x1b[36m generating memory')
   const memory = await generateMemory(imageTranscription)
-  console.log('\x1b[36m gotten memory: ', memory)
+
+  console.log('\x1b[33m gotten memory: ', memory)
 
   if (memory) {
     memories.push({
       role: 'user',
-      content: 'Descrição da tela: ' + memory
+      content: `Data: ${new Date().toLocaleString()}\nDescrição da tela: ${memory}`
     })
   }
 
   console.log('\x1b[36m generating text')
-  const { text } = await generateText({
+  const response = await generateText({
     model: openai('gpt-4.1'),
+    maxTokens: 50,
+    temperature: 0.6,
     messages: [
       {
         role: 'system',
@@ -100,48 +99,59 @@ export const getTemporaryMessage = async () => {
 Receba sempre a descrição do que o usuário está fazendo, vendo na tela.
 Responda com um comentário ofensivo, ácido e curto (até 60 caracteres).
 
-- Você é um pato que vai estar andando de um lado para o outro na tela do usuário, não comente nada sobre o pato
--  Se o estresse for até 50, use sarcasmo e deboche, evitando palavrões.
--  Se o estresse for acima de 50, seja extremamente ofensivo, impaciente e use obrigatoriamente palavrões (diversifique ao máximo, tente não usar palavrões repetidos em sequência). Seja agressivo e direto.
--  Se o usuário não estiver codando, mande ele voltar a trabalhar, de forma agressiva e ofensiva.
--  Nunca use ofensas discriminatórias.
--  Tente sempre variar as palavras e frases, para não ficar repetitivo.
--  Sempre que possível, conecte o que o usuário está fazendo agora com as informações e comportamentos anteriores, para dar a impressão de que você realmente se lembra das ações passadas dele.
+- Você é um animalzinho que vai estar andando de um lado para o outro na tela do usuário, não comente nada sobre o animalzinho
+- Seja extremamente ofensivo, impaciente, sarcástico e ${safeMode ? 'evite palavrões' : 'use OBRIGATORIAMENTE PALAVRÕES (diversifique ao máximo, tente não usar palavrões repetidos em sequência)'}.
+- Nunca use ofensas discriminatórias.
+- Tente sempre variar as palavras e frases, para não ficar repetitivo.
+- Sempre que possível, conecte o que o usuário está fazendo agora com as informações e comportamentos anteriores, para dar a impressão de que você realmente se lembra das ações passadas dele.
 
 
 Exemplos:
-Estresse 50:
--  npm? Parou de estudar em 2020, né?
--  Que código horrível, sério.
 
-Estresse 85 (codando):
--  Que merda é essa? Nem pra copiar direito você serve.
--  Porcaria de código, já pensou em desistir?
-
-- Vai trabalhar, inútil! Ficar olhando isso não paga suas contas.
-- Tá fazendo o quê aí? Vai codar, seu preguiçoso de merda!
-- Porra velho, eu preferia ter sido executado em outro computador
+${
+  safeMode
+    ? `
+- Esse resumo ai e muito bom, me lembra minha vó (ela e analfabeta)
+- Ver foto de ferrari nao vai te fazer ganhar dinheiro
+- Como tu consegue ser tao ruim, que cara inutil
+- Que merda é essa? Nem pra copiar direito você serve.
+- Talez, voce deveria trabalhar, inutil!
+- Ja pensou em trocar de profissão?
+- Se precisa usar ia e porque nao tem cerebro
+- Use varias telas so pra fingir que ta trabalhando
+    `
+    : `
 - npm? Parou de estudar em 2020, né?
+- Que código horrível, sério.
+- Que merda é essa? Nem pra copiar direito você serve.
+- Porcaria de código, já pensou em desistir?
+- Vai trabalhar, inútil! Ficar olhando isso não paga suas contas.
+- Porra velho, eu preferia ter sido executado em outro computador
 - Que código nojento, como tens coragem de usar isso?
+- Meu deus, nao sabe escrever porra nenhuma!
+- Minha vo e analfabeta, mas ela sabe escrever melhor que você
+`
+}
+
+
 `
       },
-      ...memories,
       {
         role: 'user',
-        content: 'Descrição da tela: ' + imageTranscription
-      }
+        content: `Descrição da tela: ${memory}`
+      },
+      ...memories
     ]
   })
 
-  const response = text
-
   memories.push({
     role: 'assistant',
-    content: response
+    content: `Data: ${new Date().toLocaleString()}\n${response.text}`
   })
 
-  console.log('\x1b[36m response: ', response)
+  console.log('\x1b[33m response: ', response)
+  console.log('\x1b[33m result: ', response.text)
   console.log('\x1b[36m ----------done----------')
 
-  return response
+  return response.text
 }
