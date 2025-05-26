6e32881b81024fa026513955d13855870e9c0035
Anderson-Soares-Martins <andersoaresmartins@gmail.com>
2025-05-26 02:05:23 -0300

    feat: enhance TabButton component with active state styling and background effects

--- src/renderer/src/components/settings/components/TabButton.tsx
+++ src/renderer/src/components/settings/components/TabButton.tsx
@@ -7,12 +7,20 @@ export function TabButton({ icon, label, isActive, onClick }: TabButtonProps): R
     <button
       onClick={onClick}
       className={cn(
-        'w-[133px] flex items-center gap-1.5 p-1 px-2 rounded-lg hover:bg-background-secondary',
-        isActive ? 'bg-background-tertiary text-primary' : 'bg-background-primary text-secondary'
+        'w-[133px] flex items-center gap-1.5 p-1 px-2 rounded-lg hover:bg-background-secondary relative overflow-hidden',
+        isActive
+          ? 'bg-background-tertiary text-primary border-t-[1px] border-secondary/40'
+          : 'bg-background-primary text-secondary'
       )}
     >
-      <div className="w-[18px] h-[18px] flex items-center justify-center">{icon}</div>
-      <span className={'text-base'}>{label}</span>
+      {isActive && (
+        <>
+          <div className="absolute z-0 -right-6 w-[80px] h-[20px] rounded-full bg-gradient-to-l from-accent-secondary to-accent-secondary/60 blur-[12px]" />
+          <div className="absolute z-0 right-0 w-[3px] h-4 rounded-l-full bg-accent-secondary" />
+        </>
+      )}
+      <div className="w-[18px] h-[18px] flex items-center justify-center relative z-10">{icon}</div>
+      <span className="text-base leading-[1.0] relative z-10">{label}</span>
     </button>
   )
 }
