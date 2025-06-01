6fed573821eeaf3c77ba7c89a49696a9bd153276
Anderson-Soares-Martins <andersoaresmartins@gmail.com>
2025-05-31 21:56:03 -0300

    feat: add openSettings API and refactor preload and App components

--- apps/app/src/renderer/src/App.tsx
+++ apps/app/src/renderer/src/App.tsx
@@ -1,20 +1,26 @@
-import { getScreenContextReply } from './ai/main'
-import Versions from './components/Versions'
+import { Pet } from "./components/pet/pet";
+// import Versions from "./components/Versions";
+// import { getScreenContextReply } from "./ai/main";
 
 export default function App(): React.JSX.Element {
-	return (
-		<>
-			<button
-				type="button"
-				className="bg-red-500"
-				onClick={async () => {
-					await getScreenContextReply()
-				}}
-			>
-				take screenshot
-			</button>
+  return (
+    <>
+      {/* <div className="min-h-screen bg-black/30">
+        <div className="fixed bottom-4 right-4">
+          <button
+            type="button"
+            className="px-4 py-2 bg-accent-primary text-white rounded-lg shadow-lg hover:bg-accent-secondary transition-colors"
+            onClick={async () => {
+              await getScreenContextReply();
+            }}
+          >
+            Capturar Tela
+          </button>
+        </div>
 
-			<Versions />
-		</>
-	)
+        <Versions />
+      </div> */}
+      <Pet />
+    </>
+  );
 }
