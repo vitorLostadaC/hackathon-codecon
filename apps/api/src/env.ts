112fcb7ab2fb1d93ed83735753cb05e50cc2882e
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-02 21:23:00 -0300

    feat: implement mongo

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -3,7 +3,8 @@ import { z } from 'zod'
 export const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']).default('development'),
   OPENAI_API_KEY: z.string(),
-  DEBUG: z.string().default('false').transform(Boolean)
+  DEBUG: z.string().default('false').transform(Boolean),
+  MONGO_URI: z.string()
 })
 
 export const env = envSchema.parse(process.env)
