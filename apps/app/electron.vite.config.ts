c48dc912d75282d9e57f7cdf2904c192ec9ba7f5
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-05 11:21:29 -0300

    feat: add build

--- apps/app/electron.vite.config.ts
+++ apps/app/electron.vite.config.ts
@@ -1,30 +1,30 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig, externalizeDepsPlugin } from "electron-vite";
-import { resolve } from "path";
+import react from '@vitejs/plugin-react'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { resolve } from 'node:path'
 
 export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()],
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()],
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        "@renderer": resolve("src/renderer/src"),
-      },
-    },
-    plugins: [react()],
-    build: {
-      rollupOptions: {
-        input: {
-          main: resolve("src/renderer/index.html"),
-          settings: resolve("src/renderer/settings.html"),
-          gear: resolve("src/renderer/gear.html"),
-        },
-      },
-    },
-    assetsInclude: ["**/*.svg"],
-  },
-});
+	main: {
+		plugins: [externalizeDepsPlugin()]
+	},
+	preload: {
+		plugins: [externalizeDepsPlugin()]
+	},
+	renderer: {
+		resolve: {
+			alias: {
+				'@renderer': resolve('src/renderer/src')
+			}
+		},
+		plugins: [react()],
+		build: {
+			rollupOptions: {
+				input: {
+					main: resolve('src/renderer/index.html'),
+					settings: resolve('src/renderer/settings.html'),
+					gear: resolve('src/renderer/gear.html')
+				}
+			}
+		},
+		assetsInclude: ['**/*.svg']
+	}
+})
