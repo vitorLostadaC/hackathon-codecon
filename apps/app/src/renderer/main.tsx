7c772c585796b5c90259f65163ee3f9f8640cf85
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-07 08:11:54 -0300

    feat: install queryClient

--- apps/app/src/renderer/main.tsx
+++ apps/app/src/renderer/main.tsx
@@ -1,11 +1,15 @@
 import './assets/main.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from 'lib/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Routes } from './routes'
 
 createRoot(document?.getElementById('root') as HTMLElement).render(
 	<StrictMode>
-		<Routes />
+		<QueryClientProvider client={queryClient}>
+			<Routes />
+		</QueryClientProvider>
 	</StrictMode>
 )
