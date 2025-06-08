fc23681f45d8ff8d380023101a20d7e39786ee81
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-08 09:47:33 -0300

    fix: lint

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod'
 
 export const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production']).default('development'),
-  OPENAI_API_KEY: z.string(),
-  DEBUG: z.string().default('false').transform(Boolean),
-  MONGO_URI: z.string(),
-  FRONTEND_URL: z.string(),
-  ABACATEPAY_API_KEY: z.string(),
-  ABACATEPAY_SECRET_KEY: z.string()
+	NODE_ENV: z.enum(['development', 'production']).default('development'),
+	OPENAI_API_KEY: z.string(),
+	DEBUG: z.string().default('false').transform(Boolean),
+	MONGO_URI: z.string(),
+	FRONTEND_URL: z.string(),
+	ABACATEPAY_API_KEY: z.string(),
+	ABACATEPAY_SECRET_KEY: z.string()
 })
 
 export const env = envSchema.parse(process.env)
