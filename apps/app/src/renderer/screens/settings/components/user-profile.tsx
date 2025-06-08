9a669b8a194e00a115d9288d4c2b1f794ecfd690
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-08 14:41:58 -0300

    fix: ui

--- apps/app/src/renderer/screens/settings/components/user-profile.tsx
+++ apps/app/src/renderer/screens/settings/components/user-profile.tsx
@@ -27,34 +27,34 @@ export function UserProfile() {
 		)
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<div className="flex flex-col gap-4">
-					<div className="text-sm flex gap-2 items-center">
-						<CoinsIcon className="size-4" />
-						{data?.credits}
-					</div>
+		<div className="flex flex-col gap-4">
+			<div className="text-sm flex gap-2 items-center">
+				<CoinsIcon className="size-4" />
+				{data?.credits}
+			</div>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
 					<div className="flex flex-col w-40 gap-0.5 bg-granite-900 border border-smoke-700 rounded-lg p-2.5 px-4 select-none cursor-pointer">
 						<span className="text-linen-200 text-sm truncate">{auth.user?.fullName ?? ''}</span>
 						<span className="text-granite-100 text-xs truncate">
 							{auth.user.primaryEmailAddress?.emailAddress ?? ''}
 						</span>
 					</div>
-				</div>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent>
-				<DropdownMenuLabel>
-					{auth.user?.firstName ?? ''} {auth.user?.lastName ?? ''}
-				</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem asChild className="w-full">
-					<SignOutButton redirectUrl="/#/settings">
-						<div className="flex items-center gap-2">
-							<LogOutIcon className="size-4" /> Sair
-						</div>
-					</SignOutButton>
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuLabel>
+						{auth.user?.firstName ?? ''} {auth.user?.lastName ?? ''}
+					</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem asChild className="w-full">
+						<SignOutButton redirectUrl="/#/settings">
+							<div className="flex items-center gap-2">
+								<LogOutIcon className="size-4" /> Sair
+							</div>
+						</SignOutButton>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
 	)
 }
