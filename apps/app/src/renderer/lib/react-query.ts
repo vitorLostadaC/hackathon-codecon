f17513e7dfa6770b34e281200bb096aafd444811
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-11 10:33:41 -0300

    feat: implement broadcastQueryClient

--- apps/app/src/renderer/lib/react-query.ts
+++ apps/app/src/renderer/lib/react-query.ts
@@ -1,3 +1,9 @@
+import { broadcastQueryClient } from '@tanstack/query-broadcast-client-experimental'
 import { QueryClient } from '@tanstack/react-query'
 
 export const queryClient = new QueryClient()
+
+broadcastQueryClient({
+	queryClient,
+	broadcastChannel: 'my-app'
+})
